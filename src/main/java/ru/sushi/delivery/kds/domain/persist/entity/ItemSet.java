80ce1175b8f31b65876b05217b8dec4b5efca54f
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-01-03 00:35:45 +0300

    rollbacked changes

--- src/main/java/ru/sushi/delivery/kds/domain/persist/entity/ItemSet.java
+++ src/main/java/ru/sushi/delivery/kds/domain/persist/entity/ItemSet.java
@@ -1,31 +1,16 @@
 package ru.sushi.delivery.kds.domain.persist.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@Entity
-@Getter
-@Setter
+import java.util.List;
+
+@Data
+@AllArgsConstructor //todo remove
 @Builder(toBuilder = true)
-@NoArgsConstructor(force = true)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ItemSet {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @OneToOne
-    @JoinColumn(name = "item_id")
-    private Item name;
+    private final Long id;
+    private final String name;
+    private final List<Item> items;
 }
