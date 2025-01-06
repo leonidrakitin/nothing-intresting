db47c50c0b848948c5f29f5cd14e6e8e8257b830
losevdmitriy <losev_dm@vk.com>
2025-01-06 20:51:10 +0300

    added initial entities (prepack) for warehouse logic

---
+++ src/main/java/ru/sushi/delivery/kds/domain/persist/entity/Prepack.java
@@ -0,0 +1,44 @@
+package ru.sushi.delivery.kds.domain.persist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.text.DecimalFormat;
+
+@Entity
+@Table(name = "prepack")
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor(force = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Prepack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "measurement_unit_id")
+    private Measurement measurementUnit;
+
+    private Double amount;
+
+    @Override
+    public String toString() {
+        return String.format("%s %s%s", name, new DecimalFormat("#.#").format(amount), measurementUnit.getName());
+    }
+}
