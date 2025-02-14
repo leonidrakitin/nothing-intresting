b9bfd7e435dccb33376f815e34cdf2911036b2ca
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-02-14 04:48:58 +0300

    item combo finished

---
+++ src/main/java/ru/sushi/delivery/kds/domain/persist/repository/ItemComboRepository.java
@@ -0,0 +1,15 @@
+package ru.sushi.delivery.kds.domain.persist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.sushi.delivery.kds.domain.persist.entity.ItemCombo;
+
+import java.util.List;
+
+@Repository
+public interface ItemComboRepository extends JpaRepository<ItemCombo, Long> {
+
+    @Query("select ic from ItemCombo ic left join fetch ic.menuItems")
+    List<ItemCombo> findAll();
+}
---
+++ src/main/resources/liquibase/backup/14.02.25.sql
@@ -0,0 +1,35 @@
+INSERT INTO public.item_combo (name) VALUES ('Сет Филадельфия');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (2, 2), (2, 3), (2, 30);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Запеченный');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (3, 23), (3, 28), (3, 27), (3, 22);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет С креветками');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (4, 3), (4, 8), (4, 19);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Хот');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (5, 19), (5, 26), (5, 11), (5, 12), (5, 4);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Я есть суши');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (6, 2), (6, 30), (6, 23), (6, 8);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Авторский');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (7, 4), (7, 29), (7, 11), (7, 21), (7, 38);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Большой');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (8, 1), (8, 9), (8, 13), (8, 7), (8, 20), (8, 26);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Суши');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (9, 31), (9, 33), (9, 32), (9, 34), (9, 35), (9, 36);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет С закусками');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (10, 2), (10, 27), (10, 39), (10, 41), (10, 38);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Эксклюзивный');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (11, 5), (11, 3), (11, 11);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет На двоих');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (12, 1), (12, 10), (12, 26), (12, 37);
+
+INSERT INTO public.item_combo (name) VALUES ('Сет Маки');
+INSERT INTO public.item_combo_compound (item_combo_id, menu_item_id) VALUES (13, 14), (13, 15), (13, 16), (13, 17), (13, 18);
