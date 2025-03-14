1bd5f36b82b6c7bc612beccf2f31cbd94d8e264e
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-03-15 02:54:26 +0300

    added RU locale for all UI components and edited default timing for kitchen
    (cherry picked from commit 354135ad7e2f9ec1135ce3459566954853ac4df2)

--- src/main/java/ru/sushi/delivery/kds/KdsApplication.java
+++ src/main/java/ru/sushi/delivery/kds/KdsApplication.java
@@ -1,8 +1,11 @@
 package ru.sushi.delivery.kds;
 
+import com.vaadin.flow.component.UI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Locale;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +13,7 @@ public class KdsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KdsApplication.class, args);
+		UI.getCurrent().setLocale(Locale.forLanguageTag("ru-RU"));
 	}
 
 }
