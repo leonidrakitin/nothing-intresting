2c34b122a47ee7d9514ee0e740f18f591cadd7fd
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-03-22 01:08:52 +0300

    feat(#726358) Исправлены API ендпоинты и контроллеры

--- src/main/java/ru/sushi/delivery/kds/domain/controller/WarehouseController.java
+++ src/main/java/ru/sushi/delivery/kds/domain/controller/WarehouseController.java
@@ -1,22 +1,13 @@
 package ru.sushi.delivery.kds.domain.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sushi.delivery.kds.domain.controller.dto.IngredientItemData;
 import ru.sushi.delivery.kds.domain.controller.dto.PrepackItemData;
-import ru.sushi.delivery.kds.domain.controller.dto.request.WriteOffRequest;
 import ru.sushi.delivery.kds.domain.service.IngredientItemService;
 import ru.sushi.delivery.kds.domain.service.PrepackItemService;
-import ru.sushi.delivery.kds.domain.service.WriteOffService;
-import ru.sushi.delivery.kds.dto.WriteOffItemDto;
 
 import java.util.List;
 
@@ -27,7 +18,6 @@ public class WarehouseController {
 
     private final IngredientItemService ingredientItemService;
     private final PrepackItemService prepackItemService;
-    private final WriteOffService writeOffService;
 
     @GetMapping("/ingredients")
     public List<IngredientItemData> getAllIngredients() {
@@ -37,21 +27,5 @@ public class WarehouseController {
     @GetMapping("/prepacks")
     public List<PrepackItemData> getAllPrepacks() {
         return prepackItemService.getAll();
-    }
-
-    @PostMapping("/write-off")
-    public void writeOff(@RequestBody WriteOffRequest writeOffRequest) {
-        writeOffService.writeOff(writeOffRequest);
-    }
-
-    @PostMapping("/add-write-off")
-    public void addWriteOff(@RequestBody WriteOffRequest writeOffRequest) {
-        writeOffService.createWriteOff(writeOffRequest);
-    }
-
-    @GetMapping("/allWriteOff")
-    public Page<WriteOffItemDto> getAllWriteOff(@RequestParam Integer p, Integer e) {
-        PageRequest pageRequest = PageRequest.of(p, e, Sort.by(Sort.Direction.DESC, "id"));
-        return writeOffService.getAll(pageRequest);
     }
 }
---
+++ src/main/java/ru/sushi/delivery/kds/domain/controller/WriteOffController.java
@@ -0,0 +1,34 @@
+package ru.sushi.delivery.kds.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+import ru.sushi.delivery.kds.domain.controller.dto.request.WriteOffRequest;
+import ru.sushi.delivery.kds.domain.service.WriteOffService;
+import ru.sushi.delivery.kds.dto.WriteOffItemDto;
+
+@RequestMapping
+@RestController
+@RequiredArgsConstructor
+public class WriteOffController {
+
+    private final WriteOffService writeOffService;
+
+    @PostMapping("/v1/write-off")
+    public void writeOff(@RequestBody WriteOffRequest writeOffRequest) {
+        writeOffService.writeOff(writeOffRequest);
+    }
+
+    @PostMapping("/v2/write-off")
+    public void addWriteOff(@RequestBody WriteOffRequest writeOffRequest) {
+        writeOffService.createWriteOff(writeOffRequest);
+    }
+
+    @GetMapping("/write-off/list")
+    public Page<WriteOffItemDto> getAllWriteOff(@RequestParam Integer p, Integer e) {
+        PageRequest pageRequest = PageRequest.of(p, e, Sort.by(Sort.Direction.DESC, "id"));
+        return writeOffService.getAll(pageRequest);
+    }
+}
