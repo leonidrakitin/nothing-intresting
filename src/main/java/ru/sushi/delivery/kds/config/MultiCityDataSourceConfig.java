c44cebe9e3e155712043aaab949c2b836f93cf03
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-11-17 01:36:42 +0300

    Тестирование мультигородов

--- src/main/java/ru/sushi/delivery/kds/config/MultiCityDataSourceConfig.java
+++ src/main/java/ru/sushi/delivery/kds/config/MultiCityDataSourceConfig.java
@@ -3,6 +3,7 @@ package ru.sushi.delivery.kds.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -10,12 +11,20 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
+/**
+ * Конфигурация для дополнительных DataSource (Парнас и Ухта).
+ * Эти DataSource используются только для чтения данных через JDBC,
+ * не конфликтуют с основным DataSource из application.yaml,
+ * который используется для JPA и EntityManagerFactory.
+ */
 @Configuration
 public class MultiCityDataSourceConfig {
 
-    // Парнас DataSource
-    @Bean
+    // Парнас DataSource - создается только для мульти-города, не конфликтует с основным
+    // Используем явное имя бина, чтобы не конфликтовать с основным DataSource
+    @Bean(name = "parnasDataSource")
     @Qualifier("parnasDataSource")
+    @ConfigurationProperties(prefix = "parnas.datasource")
     public DataSource parnasDataSource(Environment env) {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(env.getProperty("parnas.datasource.url", 
@@ -28,12 +37,15 @@ public class MultiCityDataSourceConfig {
         config.setConnectionTimeout(30000);
         config.setIdleTimeout(600000);
         config.setMaxLifetime(1800000);
+        config.setPoolName("ParnasPool");
+        // Важно: не используем этот DataSource для JPA
         return new HikariDataSource(config);
     }
 
-    // Ухта DataSource
-    @Bean
+    // Ухта DataSource - создается только для мульти-города, не конфликтует с основным
+    @Bean(name = "ukhtaDataSource")
     @Qualifier("ukhtaDataSource")
+    @ConfigurationProperties(prefix = "ukhta.datasource")
     public DataSource ukhtaDataSource(Environment env) {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(env.getProperty("ukhta.datasource.url", 
@@ -46,16 +58,18 @@ public class MultiCityDataSourceConfig {
         config.setConnectionTimeout(30000);
         config.setIdleTimeout(600000);
         config.setMaxLifetime(1800000);
+        config.setPoolName("UkhtaPool");
+        // Важно: не используем этот DataSource для JPA
         return new HikariDataSource(config);
     }
 
-    @Bean
+    @Bean(name = "parnasJdbcTemplate")
     @Qualifier("parnasJdbcTemplate")
     public JdbcTemplate parnasJdbcTemplate(@Qualifier("parnasDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 
-    @Bean
+    @Bean(name = "ukhtaJdbcTemplate")
     @Qualifier("ukhtaJdbcTemplate")
     public JdbcTemplate ukhtaJdbcTemplate(@Qualifier("ukhtaDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
